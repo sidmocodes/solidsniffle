5ca9644f1d4ec32a26c92d6480718b4f8d7c750a
unagi-pivots <siddharthmohantywk@gmail.com>
2020-04-21 15:32:29 +0530

    Update widget_test.dart name inside

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:messio/main.dart';
+import 'package:solidsniffle/main.dart';
 
 void main() {
   testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
